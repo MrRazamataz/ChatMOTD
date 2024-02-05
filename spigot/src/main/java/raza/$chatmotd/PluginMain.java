e53de959a2176eacd382f3891524ecb0713b2bb1
MrRazamataz <mrrazamataz@mrrazamataz.ga>
2024-02-05 17:51:18 +0000

    added support for tellraw json in chatmotd.json (v1.2.0)

--- spigot/src/main/java/raza/$chatmotd/PluginMain.java
+++ spigot/src/main/java/raza/$chatmotd/PluginMain.java
@@ -1,11 +1,16 @@
 package raza.$chatmotd;
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
+
 import org.bukkit.*;
 import org.bukkit.command.*;
+import org.bukkit.entity.Player;
 import org.bukkit.event.*;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.*;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.*;
 
 public class PluginMain extends JavaPlugin implements Listener {
 
@@ -15,82 +20,116 @@ public class PluginMain extends JavaPlugin implements Listener {
 	public void onEnable() {
 		instance = this;
 		getServer().getPluginManager().registerEvents(this, this);
-		PluginMain.createResourceFile("chatmotd.txt");
+		createResourceFile("chatmotd.txt");
+		new Metrics(PluginMain.getInstance(), ((int) (14685d)));
 		try {
-			for (Object FINAL_loopValue1 : java.nio.file.Files.readAllLines(
-					new File(String.valueOf(PluginMain.getInstance().getDataFolder()), "chatmotd.txt").toPath(),
-					java.nio.charset.StandardCharsets.UTF_8)) {
-				PluginMain.getInstance().getLogger().info(("The motd is set to:"
-						+ ChatColor.translateAlternateColorCodes('&', String.valueOf(FINAL_loopValue1))));
-			}
-			PluginMain.getInstance().getLogger().info("Plugin has enabled.");
-			new Metrics(PluginMain.getInstance(), ((int) (14685d)));
-			if (PluginMain.hasSpigotUpdate("100844")) {
-				PluginMain.getInstance().getLogger()
-						.warning((ChatColor.translateAlternateColorCodes('&',
-								"&6There is an update available on spigot. Version: ")
-								+ PluginMain.getSpigotVersion("100844")));
+			logMotdMessages();
+			getLogger().info("Plugin has enabled.");
+			if (hasSpigotUpdate("100844")) {
+				getLogger().info("There is a new update available on spigot! https://www.spigotmc.org/resources/chatmotd.100844/.");
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
+		if (new File(getInstance().getDataFolder(), "chatmotd.json").exists()) {
+			getLogger().info("JSON mode is enabled, you cannot edit the motd with the setchatmotd command.");
+		}
+		}
 
 	@Override
 	public void onDisable() {
-		try {
-			PluginMain.getInstance().getLogger().info("Plugin has been disabled.");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		getLogger().info("Plugin has been disabled.");
 	}
 
 	@Override
 	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] commandArgs) {
 		if (command.getName().equalsIgnoreCase("motd")) {
-			try {
-				for (Object FINAL_loopValue1 : java.nio.file.Files.readAllLines(
-						new File(String.valueOf(PluginMain.getInstance().getDataFolder()), "chatmotd.txt").toPath(),
-						java.nio.charset.StandardCharsets.UTF_8)) {
-					commandSender
-							.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(FINAL_loopValue1)));
+			if (new File(getInstance().getDataFolder(), "chatmotd.json").exists()) {
+				String jsonMessage = null;
+				try {
+					jsonMessage = String.join("", Files.readAllLines(new File(getInstance().getDataFolder(), "chatmotd.json").toPath(), StandardCharsets.UTF_8));
+					Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + commandSender.getName() + " " + jsonMessage);
+				} catch (IOException e) {
+					throw new RuntimeException(e);
 				}
+				return true;
+			}
+				try {
+				sendMotdMessages(commandSender);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			return true;
 		}
+
 		if (command.getName().equalsIgnoreCase("setchatmotd")) {
-			Object $2de0fd60f64bec84460174f6876c3f09 = null;
-			try {
-				$2de0fd60f64bec84460174f6876c3f09 = String.join(" ", PluginMain.createList(commandArgs));
-				$2de0fd60f64bec84460174f6876c3f09 = ((java.lang.String) String
-						.valueOf($2de0fd60f64bec84460174f6876c3f09)
-						.replace(String.valueOf("\\n"), String.valueOf('\n')));
-				new File(String.valueOf(PluginMain.getInstance().getDataFolder()), "chatmotd.txt").delete();
-				new File(String.valueOf(PluginMain.getInstance().getDataFolder()), "chatmotd.txt").createNewFile();
-				java.nio.file.Files.write(
-						new File(String.valueOf(PluginMain.getInstance().getDataFolder()), "chatmotd.txt").toPath(),
-						Collections.singleton(ChatColor.translateAlternateColorCodes('&',
-								String.valueOf($2de0fd60f64bec84460174f6876c3f09))),
-						java.nio.charset.StandardCharsets.UTF_8, java.nio.file.StandardOpenOption.WRITE);
-				commandSender.sendMessage(("The ChatMOTD has been set to: " + ChatColor
-						.translateAlternateColorCodes('&', String.valueOf($2de0fd60f64bec84460174f6876c3f09))));
-				org.bukkit.Bukkit.broadcastMessage(
-						ChatColor.translateAlternateColorCodes('&', String.valueOf($2de0fd60f64bec84460174f6876c3f09)));
+			if (new File(getInstance().getDataFolder(), "chatmotd.json").exists()) {
+				commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cJSON mode is enabled, you cannot edit the motd with the setchatmotd command.\n" +
+						"Please edit the chatmotd.json file in the plugin folder instead."));
+				return true;
+			}
+				try {
+				setChatMotd(commandArgs);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			return true;
 		}
+
 		return true;
 	}
 
-	public static void procedure(String procedure, List procedureArgs) throws Exception {
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		try {
+			sendMotdToPlayer(event.getPlayer());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public static Object function(String function, List functionArgs) throws Exception {
-		return null;
+	private void logMotdMessages() throws IOException {
+		for (String motdMessage : Files.readAllLines(getMotdFilePath(), StandardCharsets.UTF_8)) {
+			getLogger().info("The motd is set to: " + ChatColor.translateAlternateColorCodes('&', motdMessage));
+		}
+	}
+	private void sendMotdMessages(CommandSender commandSender) throws IOException {
+		for (String motdMessage : Files.readAllLines(getMotdFilePath(), StandardCharsets.UTF_8)) {
+			commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', motdMessage));
+		}
+	}
+	private void sendMotdToPlayer(Player player) throws IOException {
+		if (new File(getInstance().getDataFolder(), "chatmotd.json").exists()) {
+			// just get the raw json as a string
+			String jsonMessage = String.join("", Files.readAllLines(new File(getInstance().getDataFolder(), "chatmotd.json").toPath(), StandardCharsets.UTF_8));
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "tellraw " + player.getName() + " " + jsonMessage);
+			return;
+		}
+		for (String motdMessage : Files.readAllLines(getMotdFilePath(), StandardCharsets.UTF_8)) {
+			getLogger().info("this is the motd message: " + motdMessage);
+			player.sendMessage(ChatColor.translateAlternateColorCodes('&', motdMessage));
+		}
+	}
+
+	private void setChatMotd(String[] commandArgs) throws IOException {
+		String messageToSet = String.join(" ", createList(commandArgs));
+		messageToSet = messageToSet.replace("\\n", "\n");
+
+		// Delete and recreate the file
+		File motdFile = new File(getInstance().getDataFolder(), "chatmotd.txt");
+		motdFile.delete();
+		motdFile.createNewFile();
+
+		// Write the new message to the file
+		Files.write(motdFile.toPath(), Collections.singleton(ChatColor.translateAlternateColorCodes('&', messageToSet)),
+				StandardCharsets.UTF_8, StandardOpenOption.WRITE);
+
+		// Inform the sender and broadcast the message
+		CommandSender commandSender = Bukkit.getConsoleSender();
+		commandSender.sendMessage("The ChatMOTD has been set to: " +
+				ChatColor.translateAlternateColorCodes('&', messageToSet));
+		Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', messageToSet));
 	}
 
 	public static List createList(Object obj) {
@@ -113,44 +152,12 @@ public class PluginMain extends JavaPlugin implements Listener {
 		return list;
 	}
 
-	public static void createResourceFile(String path) {
-		Path file = getInstance().getDataFolder().toPath().resolve(path);
-		if (Files.notExists(file)) {
-			try (InputStream inputStream = PluginMain.class.getResourceAsStream("/" + path)) {
-				Files.createDirectories(file.getParent());
-				Files.copy(inputStream, file);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+	private Path getMotdFilePath() {
+		return new File(getInstance().getDataFolder(), "chatmotd.txt").toPath();
 	}
 
 	public static PluginMain getInstance() {
 		return instance;
-	}
-
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void event1(org.bukkit.event.player.PlayerJoinEvent event) throws Exception {
-		for (Object FINAL_loopValue1 : java.nio.file.Files.readAllLines(
-				new File(String.valueOf(PluginMain.getInstance().getDataFolder()), "chatmotd.txt").toPath(),
-				java.nio.charset.StandardCharsets.UTF_8)) {
-			((org.bukkit.command.CommandSender) (Object) ((org.bukkit.entity.Player) event.getPlayer()))
-					.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(FINAL_loopValue1)));
-		}
-	}
-
-	public static String getSpigotVersion(String resourceId) {
-		String newVersion = PluginMain.getInstance().getDescription().getVersion();
-		try (java.io.InputStream inputStream = new java.net.URL(
-				"https://api.spigotmc.org/legacy/update.php?resource=" + resourceId).openStream();
-				java.util.Scanner scanner = new java.util.Scanner(inputStream)) {
-			if (scanner.hasNext())
-				newVersion = String.valueOf(scanner.next());
-		} catch (java.io.IOException ioException) {
-			newVersion = "Sal4iDev#4767";
-			ioException.printStackTrace();
-		}
-		return newVersion;
 	}
 
 	public static boolean hasSpigotUpdate(String resourceId) {
@@ -165,5 +172,16 @@ public class PluginMain extends JavaPlugin implements Listener {
 			hasUpdate = false;
 		}
 		return hasUpdate;
+	}
+	public static void createResourceFile(String path) {
+		Path file = getInstance().getDataFolder().toPath().resolve(path);
+		if (Files.notExists(file)) {
+			try (InputStream inputStream = PluginMain.class.getResourceAsStream("/" + path)) {
+				Files.createDirectories(file.getParent());
+				Files.copy(inputStream, file);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
